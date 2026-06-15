37c208302d3902dc751e79d05d87382fd1704f35
jadikhalid <jadikhalid@gmail.com>
2026-06-15 11:36:45 +0100

    going on

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,15 @@
 const express = require("express");
+const dotenv = require("dotenv");
+dotenv.config();
 
 const app = express();
+
+const PORT = process.env.PORT || 5000;
 
 app.get("/", (req, res) => {
   res.send("Hello from the backend");
 });
 
-app.listen(5000, () => {
-  console.log("Server is running on port 5000");
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
